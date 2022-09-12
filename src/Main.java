cdd00f224542739de9aed3178a522bca5f834bd3
leonardofalango <leo_falango@hotmail.com>
2022-09-12 20:37:11 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Trabalhadora trabalhadora = new Trabalhadora();
 
+        System.out.println(trabalhadora.getVector());
+        trabalhadora.makeVector(10);
+        System.out.println(trabalhadora.getVector());
+        trabalhadora.bubble_sort();
+        System.out.println(trabalhadora.getVector());
     }
 }
