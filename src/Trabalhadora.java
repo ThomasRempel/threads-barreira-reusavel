52cdc466ffa0847c420b4796e3faaeb04cd596de
leonardofalango <leo_falango@hotmail.com>
2022-09-12 20:34:49 -0300

    feat makeVector, sorts, WriteFile
    ta ai leozin

--- src/Trabalhadora.java
+++ src/Trabalhadora.java
@@ -1,7 +1,86 @@
-public class Trabalhadora extends Thread{
-    public Trabalhadora(){
+import java.util.Random;
 
+public class Trabalhadora extends Thread{
+    private int[] vector;
+    private int[] sorted_vector;
+
+    public int[] makeVector(int quant){
+        int[] v = new int[quant];
+        for (int i = 0; i<quant; i++){
+            Random rand = new Random();
+            v[i] = rand.nextInt(106);
+        }
+        return v;
     }
+
+    public void bubble_sort(){
+        for (int i = 0; i < this.sorted_vector.length; i++){
+            for (int j = 0; j < this.sorted_vector.length - 1; j++){
+                if (this.sorted_vector[j] > this.sorted_vector[j+1]){
+                    exchange(i, j);
+                }
+            }
+        }
+    }
+
+    private void quicksort(int low, int high) {
+        int i = low, j = high;
+        int pivot = this.sorted_vector[low + (high-low)/2];
+        while (i <= j) {
+            while (this.sorted_vector[i] < pivot) {
+                i++;
+            }
+            while (this.sorted_vector[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                exchange(i, j);
+                i++;
+                j--;
+            }
+        }
+        // Recursivamente
+        if (low < j)
+            quicksort(low, j);
+        if (i < high)
+            quicksort(i, high);
+    }
+    private void exchange(int i, int j) {
+        int temp = this.sorted_vector[i];
+        this.sorted_vector[i] = this.sorted_vector[j];
+        this.sorted_vector[j] = temp;
+    }
+
+
+    public Trabalhadora(){
+        this.vector = makeVector(100);
+    }
+
+    public String getVector() {
+        String str = "";
+        for (int i=0; i<this.vector.length; i++){
+            str += this.vector[i] + " ";
+        }
+        return str;
+    }
+
+    public String getSortedVector() {
+        String str = "";
+        for (int i = 0; i<this.sorted_vector.length; i++)str+=this.sorted_vector[i] + " ";
+        return str;
+    }
+
+    public void setVector(int[] vector) {
+        this.vector = vector;
+    }
+
+    public void makeArquivoDesordenado(){
+        WriteFile.WriteFilePath("numerosDesordenados.txt", getVector());
+    }
+    public void makeArquivoOrdenado(){
+        WriteFile.WriteFilePath("numerosOrdenados.txt", getSortedVector());
+    }
+
 
     public void run(){
         try{
