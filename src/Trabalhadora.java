04d2926612ffdd4cfde9b50143ac91df9e957784
leonardo <leonardo.luize2@gmail.com>
2022-09-12 19:23:52 -0300

    feat: adiciona classe trabalhadora

---
+++ src/Trabalhadora.java
@@ -0,0 +1,13 @@
+public class Trabalhadora extends Thread{
+    public Trabalhadora(){
+
+    }
+
+    public void run(){
+        try{
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
