23a5f34c10392272aa94ed413d7cf8e8db77a694
Thomas Rempel <80909030+ThomasRempel@users.noreply.github.com>
2022-09-12 22:25:20 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Trabalhadora trabalhadora = new Trabalhadora();
 
-        System.out.println(trabalhadora.getVector());
-        trabalhadora.makeVector(10);
-        System.out.println(trabalhadora.getVector());
-        trabalhadora.bubble_sort();
-        System.out.println(trabalhadora.getVector());
+        trabalhadora.start();
     }
 }
+
