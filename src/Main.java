6d01a69e75003c421eed7a7943b9afd0181af55f
leonardo <leonardo.luize2@gmail.com>
2022-09-12 21:29:01 -0300

    feat: instancia classe gestorSemaforo na main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        int limit = 4;
+        int[] counter = new int[1];
 
+        GestorSemaforo gestorSemaforo = new GestorSemaforo(counter, limit);
     }
 }
